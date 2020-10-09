a4506027ff103d8a59758d82c0f382017d079374
pckinghao <1824455240@qq.com>
2020-10-09 21:27:46 +0800

    网站基本搭建

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,22 @@
   <router-view />
 </template>
 
-<script>
+<script lang="ts">
+import { provide, ref } from "vue";
+import { router } from "./router";
 
 export default {
-  name: 'App',
+  name: "App",
+  setup(){
+    const width = document.documentElement.clientWidth;
+    console.log(width)
+    const menuVisible = ref(width <= 600 ? false : true)
+    provide("menuVisible",menuVisible);//set
+    router.afterEach(() => {
+      if(width <= 600){
+        menuVisible.value = false;
+      }
+    })
+  }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,38 @@
            <h1>UI</h1>
            <h2>vue3造轮子</h2>
            <p class="actions">
-               <a>GitHub</a>
-               <a>开始</a>
+               <a href="https://pckinghao.github.io/panhaoUI/index.html">GitHub</a>
+               <router-link to="/doc">开始</router-link>
            </p>
        </div>
     </div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue"
 export default{
-    components:{Topnav}
+    components:{ Topnav },
 }
 </script>
+<style lang="scss" scopde>
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    >.actions {
+        padding: 8px 0;
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
+    }
+}
+</style>
